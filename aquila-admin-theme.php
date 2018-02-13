e4d0e48f060a3c5f7e8d224e66adb7967d339ff5
GuyPrimavera <guy@designbymito.com>
2018-02-13 11:05:28 +0000

    v2.0.2

--- aquila-admin-theme.php
+++ aquila-admin-theme.php
@@ -5,14 +5,14 @@
  * Description: The Aquila Admin Theme
  * Author: Guy Primavera
  * Author URI: https://guyprimavera.com/
- * Version: 2.0.1
+ * Version: 2.0.2
  * Text Domain: aquila-admin-theme
  * License: GPL2
  *
  * 2017 Guy Primavera
  */ 
 
-$aquilaVer = "2.01";
+$aquilaVer = "2.02";
 
 include ('scripts.php');
 include ('login/aquila-login-screen.php');
