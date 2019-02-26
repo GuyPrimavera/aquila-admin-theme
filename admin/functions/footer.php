5b0a437a4fc2e72c92615cb37ccd296c24e4ed1c
Duc Anh Pham <>
2019-02-26 17:27:09 +0100

    style improvements

--- admin/functions/footer.php
+++ admin/functions/footer.php
@@ -5,10 +5,6 @@ $aquilaOptions = get_option( 'aquila_settings' );
 if(isset($aquilaOptions['aquila_chk_hideFooter']) && $aquilaOptions['aquila_chk_hideFooter'] == 1){
 	// do nothing
 } else {
-	function aquila_admin_footer_admin () {
-		echo '<a href="https://designbymito.com/" class="mitoLink" target="_blank">design by Mito</a>';
-	}
-	add_filter('admin_footer_text', 'aquila_admin_footer_admin');
 }
 
 ?>
